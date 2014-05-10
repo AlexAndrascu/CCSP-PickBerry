3e29e0259e0a8e43a87a2f513518b68e450918f2
Howard <how2945ard@gmail.com>
2014-05-10 17:22:38 +0800

    reset

--- config/adapters.js
+++ config/adapters.js
@@ -3,7 +3,9 @@ module.exports.adapters = {
 
   mongo: {
     module: 'sails-mongo',
-    url: "mongodb://heroku_app25064655:484rth5euaujg4ba9mmudnpc1f@ds029257.mongolab.com:29257/heroku_app25064655",
+    host: 'fucknews.herokuapp.com',
+    port: 27017,
+    url: process.env.DB_URL,
     schema: true
   }
 };
