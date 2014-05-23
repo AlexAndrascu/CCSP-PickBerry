ed8a81e776a0f17b69e29112a595e0e0821c95c7
Howard <how2945ard@gmail.com>
2014-05-23 21:25:12 +0800

    mongolab

--- config/connections.js
+++ config/connections.js
@@ -7,12 +7,9 @@ module.exports.connections = {
   //   password: '',
   //   database: 'test'
   // }
-  disk:{
-    module: 'sails-disk'
-  },
   mongo: {
-    module: 'sails-mongo',
-    url: "mongodb://heroku_app25064655:484rth5euaujg4ba9mmudnpc1f@ds029257.mongolab.com:29257/heroku_app25064655",
+    adapter: 'sails-mongo',
+    url: process.env.MONGOLAB_URI,
     schema: true
   }
 };
