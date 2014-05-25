76d9a5c65e65c846f77a7fa908fb6ecdfe06aad7
Howard <how2945ard@gmail.com>
2014-05-25 23:51:31 +0800

    before api

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@
 
 (function (io) {
 
-  // as soon as this file is loaded, connect automatically, 
+  // as soon as this file is loaded, connect automatically,
   var socket = io.connect();
   if (typeof console !== 'undefined') {
     log('Connecting to Sails.js...');
@@ -34,12 +34,12 @@
 
     ///////////////////////////////////////////////////////////
     // Here's where you'll want to add any custom logic for
-    // when the browser establishes its socket connection to 
+    // when the browser establishes its socket connection to
     // the Sails.js server.
     ///////////////////////////////////////////////////////////
     log(
-        'Socket is now connected and globally accessible as `socket`.\n' + 
-        'e.g. to send a GET request to Sails, try \n' + 
+        'Socket is now connected and globally accessible as `socket`.\n' +
+        'e.g. to send a GET request to Sails, try \n' +
         '`socket.get("/", function (response) ' +
         '{ console.log(response); })`'
     );
@@ -60,12 +60,78 @@
       console.log.apply(console, arguments);
     }
   }
-  
+  angular.module("fucknews", [])
+  .controller("mainCtrl", function($scope) {
+    console.log("angularjs loaded")
 
-})(
+
+    $scope.reportUrl = {};
+    $scope.submit = function() {
+        $scope.$broadcast('event:force-model-update');
+        socket.post('/report/'+$scope.report.url),{},function(data){
+          console.log(data)
+        }
+    }
+
+    socket.on('message', function messageReceived(message) {
+      console.log(message)
+      if(message.verb== "create"){
+        $scope.lists.push(message.data);
+        $scope.$apply();
+      }
+      if(message.verb== "destroy"){
+        $scope.lists.forEach(function(list,idx){
+          if(list.id==message.id){
+            $scope.lists.splice(idx,1);
+            $scope.$apply();
+          }
+        })
+      }
+    });
+
+    // socket.get("/list", function (lists){
+
+    //   console.log(lists);
+
+    //   $scope.lists = lists;
+
+    //  $scope.$apply(); // tell the controller that angular function has callback
+
+    //  // because socket isn't a included library in angular in this project
+
+    // });
+    // $scope.create = function(){
+    //   socket.post('/list',{},function(data){
+    //     console.log(data)
+    //     $scope.lists.push(data)
+    //     $scope.$apply()
+    //   })
+    // }
+    // $scope.delete = function($index){
+    //   socket.delete('/list/'+$scope.lists[$index].id,function(data){
+    //     console.log(data)
+    //     $scope.lists.splice($index,1)
+    //     $scope.$apply()
+    //   } )
+    // }
+  }).directive('forceModelUpdate', function($compile) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            scope.$on('event:force-model-update', function() {
+                ctrl.$setViewValue(element.val());
+            });
+        }
+    }
+  });
+})
+(
 
   // In case you're wrapping socket.io to prevent pollution of the global namespace,
   // you can replace `window.io` with your own `io` here:
   window.io
 
 );
+
+
