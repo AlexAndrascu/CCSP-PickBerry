d12ef5b05a4424d1a21f45031330505ca4f2e9d3
BallChen <BallChen@BallChendeMacBook-Pro.local>
2014-05-28 18:17:25 +0800

    local

---
+++ config/local.js
@@ -0,0 +1,14 @@
+
+var config = { };
+
+// should end in /
+config.rootUrl  = process.env.ROOT_URL                  || 'http://localhost:1337/';
+
+config.facebook = {
+    appId:          process.env.FACEBOOK_APPID          || '304509973038784',
+    appSecret:      process.env.FACEBOOK_APPSECRET      || 'ff494cde96a00883bb49f357c94e576b',
+    appNamespace:   process.env.FACEBOOK_APPNAMESPACE   || 'pickberry',
+    redirectUri:    process.env.FACEBOOK_REDIRECTURI    || 'http://localhost:1337/fblogin'
+};
+
+module.exports = config;
