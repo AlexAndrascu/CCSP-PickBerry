a2614550fb32e572b99e383e099d1d087c4f8d42
Howard <how2945ard@gmail.com>
2014-06-04 20:25:34 +0800

    mongo url

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,7 @@
 module.exports.connections = {
   mongo: {
     adapter: 'sails-mongo',
-    url: process.env.MONGOLAB_URI,
+    url: 'mongodb://heroku_app25064655:484rth5euaujg4ba9mmudnpc1f@ds029257.mongolab.com:29257/heroku_app25064655',//process.env.MONGOLAB_URI,
     schema : true
   }
 };
