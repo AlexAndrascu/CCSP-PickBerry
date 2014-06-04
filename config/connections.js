752cc4012ab889fbbcd7f4ba16a79fae627d96ff
Howard <how2945ard@gmail.com>
2014-06-04 20:19:56 +0800

    local

--- config/connections.js
+++ config/connections.js
@@ -1,18 +1,8 @@
 module.exports.connections = {
-  // mongo: {
-  //   module: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: '',
-  //   password: '',
-  //   database: 'test'
-  // }
   mongo: {
     adapter: 'sails-mongo',
     url: process.env.MONGOLAB_URI,
     schema : true
   }
-
-  
 };
 
