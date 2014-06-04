55eea1438c6d71c4be395c51fbcf0be1515ad9a5
BallChen <BallChen@BallChendeMacBook-Pro.local>
2014-06-04 17:36:29 +0800

    news mail

--- api/controllers/MailerController.js
+++ api/controllers/MailerController.js
@@ -5,10 +5,53 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 var nodemailer = require("nodemailer");
+var email;
+var news;
 
 module.exports = {
 	mail_form: function(req,res){
-		res.view("mailer/mailForm")
+		
+		console.log(req.param('url'));
+		console.log(req.param('media'));
+
+		Company.findOne({name: req.param('media')})
+			.exec(function(err, data){
+				if(data)
+				{
+					email = data.email;
+					console.log(email);
+				}
+				else{
+					email = "";
+				}
+				News.findOne({url: req.param('url')}).exec(function(err, data){
+					if(data){
+						news = data.title;
+						console.log(news);
+						res.view("mailer/mailForm",{
+							id: data.id,
+							media: req.param('media'),
+							url: req.param('url'),
+							news: news,
+							email: email
+						})
+					}
+					else{
+						news = "";
+						res.view("mailer/mailForm",{
+							media: req.param('media'),
+							url: req.param('url'),
+							news: news,
+							email: email
+						})
+					}
+
+				})
+			})
+
+		
+		
+		
 	},
 	send_mail: function(req,res){
 		var smtpTransport = nodemailer.createTransport("SMTP",{
@@ -30,7 +73,7 @@ module.exports = {
 		        console.log(error);
 		    }else{
 		        console.log("Message sent: " + response.message);
-		        res.send(response)
+		        res.redirect("/news/show/"+req.body.url)
 		    }
 		    // if you don't want to use this transport object anymore, uncomment following line
 		    smtpTransport.close(); // shut down the connection pool, no more messages
