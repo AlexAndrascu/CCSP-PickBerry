b26eae9e261c497619170883ac9b933b4b278143
Howard <how2945ard@gmail.com>
2014-06-05 10:15:23 +0800

    session

--- config/session.js
+++ config/session.js
@@ -35,7 +35,7 @@ module.exports.session = {
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
-  adapter: 'sails-mongo',
+  adapter: 'mongo',
   url: "mongodb://heroku_app25064655:484rth5euaujg4ba9mmudnpc1f@ds029257.mongolab.com:29257/heroku_app25064655",
   collection: 'sessions',
   //
